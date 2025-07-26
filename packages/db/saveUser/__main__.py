ee8af329ff27dfece3d5400cba0794d12153c33b
bonzo4 <alonzo.sabino814@gmail.com>
2025-07-26 07:49:53 -0500

    feat: save user service test

---
+++ packages/db/saveUser/__main__.py
@@ -0,0 +1,24 @@
+import os
+
+from pymongo import MongoClient
+
+def main():
+    
+    uri = os.environ.get("DATABASE_URL")
+    client = MongoClient(uri)
+    
+    try:
+        db = client["app"]
+        collection = db["users"]
+        collection.insert_one({
+            "name": "Alonzo"
+        })
+        return { "ok": True }
+    except Exception as e:
+        print(e)
+        return {
+            "body": { "error": "There was a problem adding the email address to the database." },
+            "statusCode": 400
+        }
+    finally:
+        client.close()
